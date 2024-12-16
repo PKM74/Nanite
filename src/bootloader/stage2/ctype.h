473dac80f2c287acef6e9259f171f0077984c641
Tyler McGurrin <mcgurrintyler@gmail.com>
2024-12-16 05:49:31 -0500

    forgort to add these lol

---
+++ src/bootloader/stage2/ctype.h
@@ -0,0 +1,10 @@
+/*----------------*\
+|Nanite OS         |
+|Copyright (C) 2024|
+|Tyler McGurrin    |
+\*----------------*/
+#pragma once
+#include "stdint.h"
+
+bool islower(char chr);
+char toupper(char chr);
